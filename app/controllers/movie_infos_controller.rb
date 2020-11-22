c3732ec2cc8f54a37fcb6a28b4b6ddfac3c946cc
yuliaBaki <61301728+yuliaBaki@users.noreply.github.com>
2020-11-22 00:25:46 -0500

    Improve display and make the code better

--- app/controllers/movie_infos_controller.rb
+++ app/controllers/movie_infos_controller.rb
@@ -10,4 +10,28 @@ class MovieInfosController < ApplicationController
   def edit
     @movie_infos = MovieInfo.find(params[:id])
   end
+
+  def update
+    #fail
+    @movie_infos = MovieInfo.find(params[:id]) #get the id of the movie
+    @movie_infos.update(event_params) 
+    redirect_to movie_info_path
+  end
+
+  def new
+    @movie_infos = MovieInfo.new
+  end
+
+  def create
+    @movie_infos = MovieInfo.new(event_params)
+    @movie_infos.save
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    movie_param = params.require(:movie_info).permit(:title, :location, :total_gross, :starts_at, :description, :rating)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :movie_infos
   root to: 'movie_infos#index'
-  get "movie_infos/:id" => 'movie_infos#show', as:"movie"
-  get "movie_infos/:id/edit" => "movie_infos#edit", as: "edit_movie"
+
+  #get replaced by resources :movie_infos
+  #get "movie_infos/:id" => "movie_infos#show", as:"movie"
+  #get "movie_infos/new" => "movie_infos#new"
+  #get "movie_infos/:id/edit" => "movie_infos#edit", as: "edit_movie"
+  #patch "movie_infos/:id" => "movie_infos#update"
 end
