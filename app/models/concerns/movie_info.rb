180f69aa4b889e68f3e7af9d5308353404665abc
yuliaBaki <61301728+yuliaBaki@users.noreply.github.com>
2020-12-22 01:44:33 -0500

    add view and function in shared filed

--- app/models/concerns/movie_info.rb
+++ app/models/concerns/movie_info.rb
@@ -4,7 +4,10 @@ class MovieInfo < ApplicationRecord
   validates :description, length: { minimum:25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   #validates :capacity, numericality: { only_integer: true, greater_than: 0 }
-  validates :image_file_name, format: { with: /\w+\.(jpg|png)\z/i, message: "Must be a JPG or PNG image" }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "Must be a JPG or PNG image"
+   }
 
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS}
